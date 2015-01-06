842830dad5be29406cd4c13fe67f9ff663b70d69
2015 Themes <2015theme@gmail.com>
2015-01-07 00:41:52 +0545

    Logo Uploader
    Added logo uploader feature.

--- functions.php
+++ functions.php
@@ -25,3 +25,22 @@ add_action( 'wp_enqueue_scripts', 'blank_2015_theme_styles' );
 function blank_2015_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 }
+
+function blank_customize_register( $wp_customize ) {
+
+	//Logo Uploader
+	$wp_customize->add_section( 'blank_logo_section' , array(
+	    'title'       => __( 'Logo', 'blank2015' ),
+	    'priority'    => 36,
+	    'description' => 'Upload a logo to replace the default site title in the header',
+	) );
+	$wp_customize->add_setting( 'blank_logo' );
+	$wp_customize->add_control( new WP_Customize_Image_Control( $wp_customize, 'blank_logo', array(
+	    'label'    => __( 'Logo', 'blank2015' ),
+	    'section'  => 'blank_logo_section',
+	    'settings' => 'blank_logo',
+	) ) );
+
+}
+
+add_action( 'customize_register', 'blank_customize_register', 12 );
